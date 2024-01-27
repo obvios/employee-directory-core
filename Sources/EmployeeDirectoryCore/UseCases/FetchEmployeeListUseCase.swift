ccd28d874b29501e1ac59997d870a0b1c043b449
Eric Palma <palmae95@gmail.com>
2024-01-27 13:50:16 -0500

    define use case

--- Sources/EmployeeDirectoryCore/UseCases/FetchEmployeeListUseCase.swift
+++ Sources/EmployeeDirectoryCore/UseCases/FetchEmployeeListUseCase.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+public class FetchEmployeeListUseCase {
+    private let repository: EmployeesRepository
+
+    public init(repository: EmployeesRepository) {
+        self.repository = repository
+    }
+
+    public func execute() async throws -> [Employee] {
+        return try await repository.fetchEmployees()
+    }
+}
+
