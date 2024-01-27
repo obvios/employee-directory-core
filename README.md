41aa5804ef92f40389666ba71ba980cb96cf845e
Eric Palma <palmae95@gmail.com>
2024-01-26 22:24:29 -0500

    init package

---
+++ README.md
@@ -0,0 +1,3 @@
+# EmployeeDirectoryCore
+
+A description of this package.
---
+++ Tests/EmployeeDirectoryCoreTests/EmployeeDirectoryCoreTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import EmployeeDirectoryCore
+
+final class EmployeeDirectoryCoreTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(EmployeeDirectoryCore().text, "Hello, World!")
+    }
+}
