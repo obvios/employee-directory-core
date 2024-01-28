5f0a498ad54fb0b118408c4c6db2e72f9fdff85e
Eric Palma <palmae95@gmail.com>
2024-01-28 11:33:30 -0500

    add email validation

--- Sources/EmployeeDirectoryCore/UseCases/UpdateEmployeeInformationUseCase.swift
+++ Sources/EmployeeDirectoryCore/UseCases/UpdateEmployeeInformationUseCase.swift
@@ -15,6 +15,18 @@ public class UpdateEmployeeInformationUseCase {
     }
 
     public func execute(employee: Employee) async throws {
+        try validateEmail(for: employee)
         try await repository.updateEmployeeInformation(employee: employee)
     }
+    
+    private func validateEmail(for employee: Employee) throws {
+        let expectedEmail = "\(employee.firstName).\(employee.lastName)@example.com"
+        guard employee.email.lowercased() == expectedEmail.lowercased() else {
+            throw ValidationError.invalidEmail
+        }
+    }
+}
+
+enum ValidationError: Error {
+    case invalidEmail
 }
